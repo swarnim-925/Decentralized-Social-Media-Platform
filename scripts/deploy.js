fc2d515ad7b5826698300a6fbed365de55eb2757
Swarnim Sharma <135423817+swarnim-925@users.noreply.github.com>
2025-07-07 16:30:25 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,18 @@
 
+const hre = require("hardhat");
+
+async function main() {
+  const Social = await hre.ethers.getContractFactory("DecntralizedSocialMediaPlatform");
+  const social = await Social.deploy();
+
+  await social.deployed();
+
+  console.log("DecntralizedSocialMediaPlatform deployed to:", social.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
